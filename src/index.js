6edb8b1e2a79d97d0b0db4b95b96324fded3558c
thejennify <60758016+thejennify@users.noreply.github.com>
2020-10-02 03:43:27 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
+//import teh React and ReactDOM libraries 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+
+//display react compotent 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
