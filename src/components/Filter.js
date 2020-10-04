d150a1b82ae19dc6e78d562a3fd037734823d8e8
thejennify <60758016+thejennify@users.noreply.github.com>
2020-10-04 12:05:24 -0500

    animation-modal

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,21 +4,22 @@ import '../Filter.scss';
 class Filter extends Component {
 
     render() {
+        const defaultItem = 'item'
         return (
             <div className='filter'>
                 <div className='filter-result'> 
-                    {`${this.props.count} ${this.props.item}`} 
+                    {`${this.props.count} ${this.props.item ? this.props.item : defaultItem }`} 
                 </div>
                 <div className='filter-sort'> order
                     <select value={this.props.sortRange} onChange={this.props.sortProducts} className='standard-select'>
-                        <option value=''>Latest</option>
+                        <option value=''>latest</option>
                         <option value='lowest'>lowest</option>
                         <option value='highest'>highest</option>
                     </select>
                 </div>
-                <div className='filter-type'>Filter
+                <div className='filter-type'> filter
                     <select value={this.props.type} onChange={this.props.filterProducts} className='standard-select'>
-                        <option value=''>ALL</option>
+                        <option value=''>all</option>
                         <option value='dress'>dresses</option>
                         <option value='tops'>tops</option>
                         <option value='jacket'>jackets</option>
--- src/components/Products.js
+++ src/components/Products.js
@@ -2,16 +2,33 @@ import React, { Component } from 'react';
 import '../Products.scss';
 import '../index.scss';
 import formatCurrency from './util';
+import Fade from 'react-reveal/Fade';
+import Modal from 'react-modal';
+import Zoom from 'react-reveal/Zoom';
 
 class Products extends Component {
+    state={
+        product: null
+    };
+
+    openModal = (product) =>{
+        this.setState({product});
+    };
+
+    closeModal =() =>{
+        this.setState({product: null})
+    }
+
     render() {
+        const product= this.state.product
         return (
             <div>
+            <Fade bottom cascade>
               <ul className='products'>
                 {this.props.products.map(product => (
                     <li key={product._id}>
                     <div className ='product'>
-                    <a href={'#'+ product._id}>
+                    <a href={'#'+ product._id} onClick={() => this.openModal(product)}>
                         <img src={product.image} alt={product.title} />
                         <p className='title'>{product.title}</p>
                     </a>
@@ -24,6 +41,40 @@ class Products extends Component {
                     </li>
                 ))}
               </ul>  
+              </Fade>
+              {product && (
+                  <Modal isOpen ={true} onRequestClose={this.closeModal}>
+                    <Zoom>
+                    <button className='button' onClick={this.closeModal}>X</button>
+                    <div className='product-info'>
+                        <img src={product.image} alt={product.title}/>
+                        <div className='info-right'>
+                            <div className='info-top'>
+                                <p class='title'>{product.title}</p>
+                                <p>{formatCurrency(product.price)}</p>
+                            </div>
+                            <div>
+                                <p className='info-size'>
+                                    Size:{product.sizes.map(size =>(
+                                        <span>
+                                            <button className='button size'>{size}</button>
+                                        </span>
+                                    ))}
+                                </p>
+                            </div>
+                            <button className='button full' onClick={() => {
+                                this.props.addToCart(product);
+                                this.closeModal();
+                            }}>
+                                {'add to cart'.toLocaleUpperCase()}
+                            </button>
+                            <div className='product-info-description'>
+                                <p>{product.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                    </Zoom>
+                </Modal>)}
             </div>
         )
     }
