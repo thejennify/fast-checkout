0097429ab1de29ad231c2e0ede81bf39a10ee354
thejennify <60758016+thejennify@users.noreply.github.com>
2020-10-05 11:35:51 -0500

    Updates

---
+++ precache-manifest.9681f5214f4de1089434dd60f7f37e6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8a374f49ed26947e9be7df6b6fb9ff",
+    "url": "/thejennify/fast-checkout/index.html"
+  },
+  {
+    "revision": "1c100245a8ff1c4e2a8b",
+    "url": "/thejennify/fast-checkout/static/css/main.56f71c31.chunk.css"
+  },
+  {
+    "revision": "81160c52d6b3a3a4f43a",
+    "url": "/thejennify/fast-checkout/static/js/2.f15fb343.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/thejennify/fast-checkout/static/js/2.f15fb343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c100245a8ff1c4e2a8b",
+    "url": "/thejennify/fast-checkout/static/js/main.b0c7d826.chunk.js"
+  },
+  {
+    "revision": "092f979f7ea50fe22a1a",
+    "url": "/thejennify/fast-checkout/static/js/runtime-main.4a00ba5a.js"
+  }
+]);
